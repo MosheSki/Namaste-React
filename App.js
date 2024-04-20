ca6477e35fb723bbc2377f2b45d21e2ffc9364fb
MosheSki <MoshLago@gmail.com>
2024-04-20 21:46:28 +0300

    JSX&CSS exercise

--- App.js
+++ App.js
@@ -1,31 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello");
+const Logo = () => {
+  return <h1 className="logo">🔍</h1>;
+};
 
-// JSX - React Element
-const jsx = (
-  <h1 id="heading" className="head">
-    This is JSX 👌
-  </h1>
-);
+const SearchBar = () => {
+  return <input className="search" type="text" placeholder="Search" />;
+};
 
-// React Functional Component
-const Title = () => <h1 className="head">This is Title</h1>;
+const UserIcon = () => {
+  return <h1 className="userIcon">👦</h1>;
+};
 
-// Component Composition
-const HeadingComponent = () => {
+const Header = () => {
   return (
-    <div id="container">
-      {jsx}
-      <Title />
-      {Title()}
-      <h1>React Functional Component</h1>;
+    <div className="container">
+      <Logo />
+      <SearchBar />
+      <UserIcon />
     </div>
   );
 };
 
+// const heading = React.createElement("h1", { id: "heading" }, "Hello");
+
+// // JSX - React Element
+// const jsx = (
+//   <h1 id="heading" className="head">
+//     This is JSX 👌
+//   </h1>
+// );
+
+// // React Functional Component
+// const Title = () => <h1 className="head">This is Title</h1>;
+
+// // Component Composition
+// const HeadingComponent = () => {
+//   return (
+//     <div id="container">
+//       {jsx}
+//       <Title />
+//       {Title()}
+//       <h1>React Functional Component</h1>;
+//     </div>
+//   );
+// };
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // root.render(jsx);
-root.render(<HeadingComponent />);
+root.render(<Header />);

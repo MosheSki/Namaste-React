0252a3bb7f6a0a00d632bcf07d6ffacb54321f66
MosheSki <MoshLago@gmail.com>
2024-04-27 17:07:35 +0300

    episode-06

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,25 +1,75 @@
 import RestaurantCard from "./RestaurantCard.js";
-import resList from "../utils/mockData.js";
-import { useState } from "react"; //React Hook
+
+import { useState, useEffect } from "react"; //React Hooks
+import Shimmer from "./Shimmer.js";
+
+function filterData(searchText, listOfRestaurants) {
+  const filterData = listOfRestaurants.filter((restaurant) =>
+    restaurant?.info?.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+  return filterData;
+}
 
 const Body = () => {
-  //Local State Variable
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
 
-  // //Normal JS Variable
-  // let resList = [];
+  const [searchText, setSearchText] = useState("");
 
-  return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6126255&lng=77.04108959999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    //Optional Chaining
+    setListOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  // if (listOfRestaurants.length === 0) {
+  //   return <Shimmer />;
+  // }
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
-      {/* <div className="search">Search</div> */}
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        ></input>
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchText, listOfRestaurants);
+            setFilteredRestaurant(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRestaurants.filter(
-              (restaurant) => restaurant.venue.rating.score >= 8.8
+              (restaurant) => restaurant.info.avgRating > 4
             );
-
             setListOfRestaurants(filteredList);
           }}
         >
@@ -27,8 +77,8 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
-          <RestaurantCard key={restaurant.venue.id} resData={restaurant} />
+        {filteredRestaurant.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,15 +1,24 @@
+import { CDN_URL } from "../utils/constants";
+
 const RestaurantCard = (props) => {
   const { resData } = props;
 
-  const { name, tags, rating, estimate } = resData?.venue;
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    costForTwo,
+    avgRating,
+    deliveryTime,
+  } = resData?.info;
 
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-      <img className="res-logo" alt="logo" src={resData.image.url} />
+      <img className="res-logo" alt="logo" src={CDN_URL + cloudinaryImageId} />
       <h3>{name}</h3>
-      <h4>"{tags.join(", ")}"</h4>
-      <h4>{rating.score}⭐</h4>
-      <h4>estimate delivery: {estimate}min</h4>
+      <h4>"{cuisines.join(", ")}"</h4>
+      <h4>{avgRating}⭐</h4>
+      <h4>estimate delivery: {resData.info.sla.deliveryTime}min</h4>
     </div>
   );
 };
